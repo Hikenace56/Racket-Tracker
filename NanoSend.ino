3754ed00cb95a453c0ef8d0e4cb5f32bc363f401
yassine ouloum <43016405+Hikenace56@users.noreply.github.com>
2022-12-23 23:31:30 -0500

    Add files via upload

---
+++ NanoSend.ino
@@ -0,0 +1,83 @@
+#include <Arduino_LSM9DS1.h>
+#include <Wire.h>
+#include <MPU6050.h>
+
+void setup() {
+  pinMode(3,OUTPUT); //sets Dpin 11 to output
+  pinMode(2,OUTPUT); //sets Dpin 10 to output
+
+  Serial.begin(9600);
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)){
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+    while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("PC Side Connected");
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Bluetooth Side Connected");
+  Serial.println("AT"); // just a check
+}
+
+void checkSettings()
+{
+  Serial.println();
+  
+  Serial.print(" * Sleep Mode:        ");
+  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
+  
+  Serial.print(" * Clock Source:      ");
+  switch(mpu.getClockSource())
+  {
+    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
+    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
+    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
+    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
+    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+  }
+  Serial.print(" * Gyroscope:         ");
+  switch(mpu.getScale())
+  {
+    case MPU6050_SCALE_2000DPS:        Serial.println("2000 dps"); break;
+    case MPU6050_SCALE_1000DPS:        Serial.println("1000 dps"); break;
+    case MPU6050_SCALE_500DPS:         Serial.println("500 dps"); break;
+    case MPU6050_SCALE_250DPS:         Serial.println("250 dps"); break;
+  } 
+  
+  Serial.print(" * Gyroscope offsets: ");
+  Serial.print(mpu.getGyroOffsetX());
+  Serial.print(" / ");
+  Serial.print(mpu.getGyroOffsetY());
+  Serial.print(" / ");
+  Serial.println(mpu.getGyroOffsetZ());
+  
+  Serial.println();
+}
+void loop() {
+  float accelerationX, accelerationY, accelerationZ, gyroX, gyroY, gyroZ;
+  float table[6];
+  Vector rawGyro = mpu.readRawGyro();
+  Vector normGyro = mpu.readNormalizeGyro();
+
+
+  if(IMU.accelerationAvailable()){
+    IMU.readAcceleration( accelerationX, accelerationY, accelerationZ );
+    IMU.readGyroscope( gyroX, gyroY, gyroZ );
+}
+  table[0]=accelerationX;
+  table[1]=accelerationY;
+  table[2]= accelerationZ;
+  table[3]=gyroX;
+  table[4]=gyroY;
+  table[5]=gyroZ;
+
+  Serial.write(table);
+}
